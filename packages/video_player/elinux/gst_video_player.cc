da0b15e8a8e9da54f09f1b066379b77bd6fc0fad
Will Doughty <wdoughty@vermeer.com>
2024-05-29 10:10:16 -0500

    start off

--- packages/video_player/elinux/gst_video_player.cc
+++ packages/video_player/elinux/gst_video_player.cc
@@ -448,7 +448,7 @@ bool GstVideoPlayer::CreatePipeline() {
   }
   gst_bus_set_sync_handler(gst_.bus, HandleGstMessage, this, NULL);
 
-  g_object_set(G_OBJECT(gst_.fpssink), "text-overlay", TRUE, NULL);
+  g_object_set(G_OBJECT(gst_.fpssink), "text-overlay", FALSE, NULL);
   g_object_set(G_OBJECT(gst_.fpssink), "sync", FALSE, NULL);
   g_object_set(G_OBJECT(gst_.fpssink), "video-sink", gst_.video_sink, NULL);
 
